816d8521056e217a316ba8f44790e08010d0a285
alexandra_tavakelian <alexandra.tavakelian@ehess.fr>
2021-04-26 12:10:09 +0200

    Protocol outlined

--- alex-tavakelian-code.py
+++ alex-tavakelian-code.py
@@ -10,7 +10,7 @@ WHITE = (255, 255, 255)
 
 MAX_RESPONSE_DURATION = 2000
 
-trials = [('')]
+
 
 # Assigning values to X and Y
 display_width = 800
@@ -37,4 +37,30 @@ while True:
 			quit()
 
 	pygame.display.update()
+
+
+# What I need to create:
+# 10 (repetitions) x 3 (duration conditions) x 2 (partial, whole report) = 60 lists of random combinations # of 6 characters (3 letters, 3 digits, no specific order)
+# 4 "clock" conditions for the different display durations: a 60msec clock, a 90msec one, a 120msec and a 500msec 
+
+# Then, I can launch the experiment itself on the screen surface. All displays (instructions + stimuli) will be displayed at the center of the screen, in a white font.
+
+# Display the instructions for partial report, ask to press a key to display first series of stimuli
+
+# Display 60msec condition stimuli (10 stimuli (each is a random combinations of 6 characters) for 60msec + a blank mask for 500msec after each display)
+
+# Display 90msec condition stimuli (10 other stimuli for 90msec + mask)
+
+# Display 120 msec condition stimuli (10 other stimuli for 120msec + mask)
+
+
+
+# Display the instructions for whole report, ask to press a key to display first series of stimuli
+
+# Display 60msec condition stimuli (10 stimuli (each is a random combinations of 6 characters) for 60msec + a blank mask for 500msec after each display)
+
+# Display 90msec condition stimuli (10 other stimuli for 90msec + mask)
+
+# Display 120 msec condition stimuli (10 other stimuli for 120msec + mask)
+
 
